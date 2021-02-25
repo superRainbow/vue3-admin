65aefa0d8e7e3c792c697462b20de17babfe7832
superRainbow <g901612002@hotmail.com>
2021-02-25 16:23:33 +0800

    feat(store): add sidebar、loading、MenuOpend、dialogConfig

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,84 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
+import { ElLoading } from 'element-plus';
+import { setLocalStorage } from '@/utils/localStorage.ts';
+
+import router from '@/router';
+import api from '@/utils/api';
+
+const errorHandler = ({ response }: any) => {
+  const { status, data } = response;
+  console.log('status', status);
+  alert(data.message || '發生錯誤，請稍後再試');
+};
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    isLoading: false,
+    isMenuOpen: true,
+    isDialogShow: false,
+    dialogConfig: {},
+    sidebarList: []
+  },
+  getters: {
+    isLoading: state => state.isLoading,
+    isMenuOpen: state => state.isMenuOpen,
+    isDialogShow: state => state.isDialogShow,
+    dialogConfig: state => state.dialogConfig,
+    sidebarList: state => state.sidebarList
+  },
+  mutations: {
+    UPDATE_LOADING(state, flag) {
+      state.isLoading = flag;
+    },
+    UPDATE_MENU_OPEN(state) {
+      state.isMenuOpen = !state.isMenuOpen;
+    },
+    UPDATE_DIALOG_OPEN(state, { isDialogShow, config }) {
+      state.isDialogShow = isDialogShow;
+      state.dialogConfig = config ? config : {};
+    },
+    SET_SIDEBAR_LIST(state, data) {
+      state.sidebarList = data;
+      setLocalStorage('sidebarList', data);
+    }
+  },
+  actions: {
+    handLoading({ commit }, flag) {
+      ElLoading.service({ fullscreen: flag });
+      commit('UPDATE_LOADING', flag);
+    },
+    handLogin({ commit }, { data }) {
+      commit('UPDATE_LOADING', true);
+      axios
+        .post(api.LOGIN, data)
+        .then(res => {
+          if (res.data.success) {
+            commit('SET_SIDEBAR_LIST', res.data.data.menu);
+            router.push({ name: 'home' });
+          } else {
+            commit('UPDATE_DIALOG_OPEN', {
+              isDialogShow: true,
+              config: {
+                isCancelShow: false,
+                message: res.data.message
+              }
+            });
+          }
+        })
+        .catch(error => {
+          errorHandler(error);
+        })
+        .finally(() => {
+          commit('UPDATE_LOADING', false);
+        });
+    },
+    setSidebarList({ commit }, data) {
+      commit('SET_SIDEBAR_LIST', data);
+    },
+    toggleDialog({ commit }, flag) {
+      commit('UPDATE_DIALOG_OPEN', { isDialogShow: flag });
+    }
+  },
   modules: {}
 });
