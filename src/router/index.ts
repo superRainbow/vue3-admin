2e5fbafee9a3b6b7d94eda7528630b0b7747ecbd
rainbow <g901612002@hotmail.com>
2021-03-11 17:11:50 +0800

    fix(router): router setting change to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import { isAuthenticated } from '@/utils/validate';
 import Login from '@/views/Login.vue';
 import Layout from '@/layout/index.vue';
@@ -26,7 +26,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 });
 
