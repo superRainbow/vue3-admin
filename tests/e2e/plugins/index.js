7f8e68dbf42282eaa694e2d513a0e03d92499238
rainbow <g901612002@hotmail.com>
2021-05-28 20:41:43 +0800

    build(cypress): setting 調整

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -7,19 +7,23 @@
 // https://docs.cypress.io/api/plugins/preprocessors-api.html#Examples
 
 // /* eslint-disable import/no-extraneous-dependencies, global-require */
-// const webpack = require('@cypress/webpack-preprocessor')
+// 使用 webpack 預處理文件
+const webpack = require('@cypress/webpack-preprocessor');
 
 module.exports = (on, config) => {
-  // on('file:preprocessor', webpack({
-  //  webpackOptions: require('@vue/cli-service/webpack.config'),
-  //  watchOptions: {}
-  // }))
+  on(
+    'file:preprocessor',
+    webpack({
+      webpackOptions: require('@vue/cli-service/webpack.config'),
+      watchOptions: {}
+    })
+  );
 
   return Object.assign({}, config, {
-    fixturesFolder: "tests/e2e/fixtures",
-    integrationFolder: "tests/e2e/specs",
-    screenshotsFolder: "tests/e2e/screenshots",
-    videosFolder: "tests/e2e/videos",
-    supportFile: "tests/e2e/support/index.js"
+    fixturesFolder: 'tests/e2e/fixtures',
+    integrationFolder: 'tests/e2e/specs',
+    screenshotsFolder: 'tests/e2e/screenshots',
+    videosFolder: 'tests/e2e/videos',
+    supportFile: 'tests/e2e/support/index.js'
   });
 };
