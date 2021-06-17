0d9c9589bdd1c3f0e1582302daf941091f337e1f
rainbow <g901612002@hotmail.com>
2021-06-17 11:00:55 +0800

    feat(vue-config): 解決 dev 時 Invalid Host header

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-admin/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-admin/' : '/',
+  devServer: {
+    disableHostCheck: true
+  }
 };
