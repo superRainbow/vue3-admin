af45684f350a00cde9fd2493f751e1bbe7112030
rainbow <g901612002@hotmail.com>
2021-06-28 22:16:39 +0800

    refactor(vuex): 抽成 modules

---
+++ src/store/General/index.ts
@@ -0,0 +1,129 @@
+import { Module } from 'vuex';
+import router from '@/router';
+import { setLocalStorage } from '@/utils/localStorage';
+import { apiPostLogin, apiPostRefreshToken } from '@/api';
+
+const general: Module<any, any> = {
+  state: {
+    isLoading: false,
+    loadingTarget: '',
+    isMenuOpen: true,
+    isDialogShow: false,
+    dialogConfig: {},
+    user: {},
+    sidebarList: [],
+    jwt: {
+      token: '',
+      refreshToken: ''
+    }
+  },
+  getters: {
+    isLoading: state => state.isLoading,
+    loadingTarget: state => state.loadingTarget,
+    isMenuOpen: state => state.isMenuOpen,
+    isDialogShow: state => state.isDialogShow,
+    dialogConfig: state => state.dialogConfig,
+    userData: state => state.user,
+    sidebarList: state => state.sidebarList,
+    jwt: state => state.jwt
+  },
+  mutations: {
+    UPDATE_LOADING(state, { flag, target = 'content' }) {
+      state.isLoading = flag;
+      state.loadingTarget = target;
+    },
+    UPDATE_MENU_OPEN(state) {
+      state.isMenuOpen = !state.isMenuOpen;
+    },
+    UPDATE_DIALOG_OPEN(state, { flag, config }) {
+      state.isDialogShow = flag ? true : false;
+      state.dialogConfig = config ? config : {};
+    },
+    SET_SIDEBAR_LIST(state, data) {
+      state.sidebarList = data;
+      setLocalStorage('sidebarList', data);
+    },
+    SET_TOKEN(state, data) {
+      const jwt = data ? data : { token: '', refreshToken: '' };
+      state.jwt = jwt;
+      setLocalStorage('token', jwt.token);
+      setLocalStorage('refreshToken', jwt.refreshToken);
+    },
+    SET_USER_DATA(state, data) {
+      state.user = data;
+      setLocalStorage('user', data);
+    },
+    SET_ACTION_NAME(state, string) {
+      setLocalStorage('actionName', string);
+    }
+  },
+  actions: {
+    async handLogin({ commit, dispatch }, { data }) {
+      commit('UPDATE_LOADING', { flag: true, target: 'fullscreen' });
+      try {
+        dispatch('setLoginAction', await apiPostLogin(data));
+      } catch (error) {
+        console.log('error', error);
+        dispatch('toggleDialog', {
+          flag: true,
+          config: {
+            isCancelShow: false,
+            message: error.message
+          }
+        });
+      }
+      commit('UPDATE_LOADING', { flag: false });
+    },
+    async refreshToken({ commit }) {
+      try {
+        commit('SET_TOKEN', await apiPostRefreshToken());
+      } catch (error) {
+        console.log('error', error);
+      }
+    },
+    setLoginAction({ commit }, data) {
+      commit('SET_TOKEN', data);
+      commit('SET_SIDEBAR_LIST', [
+        {
+          hidden: false,
+          path: 'todo',
+          meta: {
+            title: 'todo',
+            icon: 'el-icon-s-order'
+          }
+        },
+        {
+          hidden: false,
+          path: 'https://google.com.tw',
+          meta: {
+            title: '外部連結',
+            icon: 'el-icon-link'
+          }
+        }
+      ]);
+      commit('SET_USER_DATA', {
+        roles: ['admin'],
+        avatar: 'https://avatars.githubusercontent.com/u/35356592',
+        name: 'rainbow'
+      });
+      router.push({ name: 'home' });
+    },
+    logout({ commit }) {
+      commit('SET_TOKEN');
+      commit('SET_SIDEBAR_LIST', []);
+      commit('SET_USER_DATA', {});
+      router.push({ name: 'login' });
+    },
+    setSidebarList({ commit }, data) {
+      commit('SET_SIDEBAR_LIST', data);
+    },
+    setUserData({ commit }, data) {
+      commit('SET_USER_DATA', data);
+    },
+    toggleDialog({ commit }, { flag, config }) {
+      commit('UPDATE_DIALOG_OPEN', { flag, config });
+    }
+  }
+};
+
+export default general;
---
+++ src/store/Todo/index.ts
@@ -0,0 +1,88 @@
+import { Module } from 'vuex';
+import { apiGetTodoList, apiDeleteTodoItem, apiPostTodoItem, apiPutTodoItem } from '@/api';
+
+const todo: Module<any, any> = {
+  namespaced: true,
+  state: {
+    list: [],
+    isModalShow: false,
+    modalConfig: {},
+    modalData: {}
+  },
+  getters: {
+    list: state => state.list,
+    isModalShow: state => state.isModalShow,
+    modalConfig: state => state.modalConfig,
+    modalData: state => state.modalData
+  },
+  mutations: {
+    SET_LIST(state, data: []) {
+      state.list = data.map((item: { [key: string]: any }) => {
+        item.doneString = item.done ? '已完成' : '未完成';
+        return item;
+      });
+    },
+    TOGGLE_MODAL(state, { flag, config, data }) {
+      state.isModalShow = flag ? true : false;
+      state.modalConfig = config ? config : {};
+      state.modalData = data ? data : {};
+    }
+  },
+  actions: {
+    setList({ commit }, data) {
+      commit('SET_LIST', data);
+    },
+    async getList({ commit, dispatch }) {
+      commit('UPDATE_LOADING', { flag: true }, { root: true });
+      commit('SET_ACTION_NAME', 'todo/getList', { root: true });
+      const res = await apiGetTodoList();
+      if (res) {
+        dispatch('setList', res);
+        commit('UPDATE_LOADING', { flag: false }, { root: true });
+      }
+    },
+    async addItem({ getters, commit, dispatch }, data) {
+      try {
+        commit('SET_ACTION_NAME', 'todo/addItem', { root: true });
+        const res = await apiPostTodoItem(data);
+        const newData = [...getters.list, res];
+        dispatch('setList', newData);
+      } catch (error) {
+        console.log('error', error);
+      }
+      commit('TOGGLE_MODAL', { flag: false });
+    },
+    async putItem({ getters, commit, dispatch }, data) {
+      try {
+        commit('SET_ACTION_NAME', `todo/putItem/${data.id}`, { root: true });
+        await apiPutTodoItem(data.id, data);
+        const newData = getters.list.map((item: { [key: string]: any }) => {
+          if (item.id === data.id) {
+            item = data;
+          }
+          return item;
+        });
+        dispatch('setList', newData);
+      } catch (error) {
+        console.log('error', error);
+      }
+      commit('TOGGLE_MODAL', { flag: false });
+    },
+    async deleteItem({ getters, commit, dispatch }, id: number) {
+      try {
+        commit('SET_ACTION_NAME', `todo/deleteItem/${id}`, { root: true });
+        await apiDeleteTodoItem(id);
+        const newData = getters.list.filter((item: { [key: string]: any }) => item.id !== id);
+        dispatch('setList', newData);
+      } catch (error) {
+        console.log('error', error);
+      }
+      commit('UPDATE_DIALOG_OPEN', { flag: false }, { root: true });
+    },
+    toggleModal({ commit }, { flag, config, data }) {
+      commit('TOGGLE_MODAL', { flag, config, data });
+    }
+  }
+};
+
+export default todo;
