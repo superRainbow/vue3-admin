9791fb76461eb8b0ae56f1fa1dac1c0e306fa637
rainbow <g901612002@hotmail.com>
2021-07-14 02:15:49 +0800

    build(prod): 增加 prod 環境打包程式會濾掉 console

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,27 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/vue3-admin/' : '/',
   devServer: {
     disableHostCheck: true
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              // eslint-disable-next-line @typescript-eslint/camelcase
+              drop_debugger: true,
+              // eslint-disable-next-line @typescript-eslint/camelcase
+              drop_console: true //生產環境自動刪除console
+            },
+            warnings: false
+          },
+          sourceMap: false,
+          parallel: true //使用多進程並行運行來提高構建速度。預設並發運行數：os.cpus().length - 1。
+        })
+      );
+    }
   }
 };
