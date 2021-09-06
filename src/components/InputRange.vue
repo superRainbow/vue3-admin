349e0fb31412038cf8214943e84b868393148cd8
rainbow <g901612002@hotmail.com>
2021-09-06 12:03:29 +0800

    feat(元件): 新增 inputRange 元件

---
+++ src/components/InputRange.vue
@@ -0,0 +1,67 @@
+<template>
+  <el-form-item :label="labelName"
+                :prop="propName">
+    <el-col :span="11">
+      <el-input v-model="value[0]"
+                :type="propName"
+                @blur="blurEvent"
+                placeholder="請選擇"></el-input>
+    </el-col>
+    <el-col class="line"
+            :span="2"> - </el-col>
+    <el-col :span="11">
+      <el-input v-model="value[1]"
+                :type="propName"
+                @blur="blurEvent"
+                placeholder="請選擇"></el-input>
+    </el-col>
+  </el-form-item>
+</template>
+
+<style lang="scss" scoped>
+@import '@/style/variable.scss';
+
+.line {
+  min-width: 8.3333333333%;
+  text-align: center;
+}
+</style>
+
+<script lang="ts">
+import { defineComponent, watchEffect, ref } from 'vue';
+
+export default defineComponent({
+  props: {
+    prop: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    modelValue: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props, context) {
+    const value = ref(props.modelValue);
+
+    watchEffect(() => {
+      console.log(value.value);
+    });
+
+    const blurEvent = () => {
+      context.emit('update:modelValue', value);
+    };
+
+    return {
+      propName: props.prop,
+      labelName: props.label,
+      value,
+      blurEvent,
+    };
+  },
+});
+</script>
